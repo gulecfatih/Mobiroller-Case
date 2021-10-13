6ea615608c9b4c12116d53734251247822857d0f
DESKTOP-POINQD4\gulec <gulecfatih54@gmail.com>
2021-10-13 15:18:36 +0300

    +created: Adding product entry and product listing pages +refactor: changed activity_main with activity_home_page

--- app/src/main/java/com/example/mobiroller/MainActivity.java
+++ app/src/main/java/com/example/mobiroller/MainActivity.java
@@ -2,14 +2,36 @@ package com.example.mobiroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_page);
 
+        Button productEntryButton = findViewById(R.id.productEntry);
+        Button productListingButton = findViewById(R.id.productListing);
+
+        productEntryButton.setOnClickListener(this);
+        productListingButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.productEntry:
+                startActivity( new Intent(MainActivity.this,ProductEntryActivity.class));
+                break;
+            case R.id.productListing:
+                startActivity( new Intent(MainActivity.this,ProductListActivity.class));
+                break;
+        }
     }
 }
