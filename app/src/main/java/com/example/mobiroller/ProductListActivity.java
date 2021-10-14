16ce48cf7d7e3472815389aee07c9e03571d6be4
DESKTOP-POINQD4\gulec <gulecfatih54@gmail.com>
2021-10-15 00:17:02 +0300

    +added recyclerview productlistActivity +Added recyclerview to layout

--- app/src/main/java/com/example/mobiroller/ProductListActivity.java
+++ app/src/main/java/com/example/mobiroller/ProductListActivity.java
@@ -1,17 +1,47 @@
 package com.example.mobiroller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductListActivity extends AppCompatActivity {
 
+
+    List<ProductListModel> productListModels = new ArrayList<>();
+    RecyclerView recyclerView;
+    ProductListAdapter productListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_product_list);
+        init();
+
+
+        RecyclerView();
     }
+
+    void RecyclerView(){
+        productListAdapter = new ProductListAdapter(productListModels,this);
+        recyclerView.setAdapter(productListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    void init(){
+        recyclerView = findViewById(R.id.productRecylerview);
+    }
+
+
 }
