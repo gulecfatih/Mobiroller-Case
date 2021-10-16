0714857363f51ad655a93be0b92ef9693015741a
DESKTOP-POINQD4\gulec <gulecfatih54@gmail.com>
2021-10-16 08:54:23 +0300

    +DetailScreenActivity created +activity_detail_screen created

---
+++ app/src/main/java/com/example/mobiroller/DetailScreenActivity.java
@@ -0,0 +1,54 @@
+package com.example.mobiroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailScreenActivity extends AppCompatActivity {
+
+    ImageView detailImageview;
+    TextView TextviewProductName,TextviewProductDescription,TextviewUploadDate,TextviewCategoryName,TextviewPrice;
+    String detailProductDescription,detailProductName,detailUploadDate,detailCategoryName,detailImage;
+    int detailPrice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_screen);
+
+        init();
+
+        TextviewProductName.setText(detailProductName);
+        TextviewProductDescription.setText(detailProductDescription);
+        TextviewPrice.setText(String.valueOf(detailPrice));
+        TextviewUploadDate.setText(detailUploadDate);
+        TextviewCategoryName.setText(detailCategoryName);
+        Glide.with(getApplicationContext())
+                .load(detailImage)
+                .into(detailImageview);
+
+
+    }
+
+      void init(){
+          Intent intent = getIntent();
+          detailProductName = intent.getStringExtra("productName");
+          detailProductDescription = intent.getStringExtra("productDescription");
+          detailPrice =intent.getExtras().getInt("price"); // hata
+          detailUploadDate = intent.getStringExtra("uploadDate");
+          detailCategoryName = intent.getStringExtra("categoryName");
+          detailImage =intent.getStringExtra("img");
+
+
+          TextviewProductName =findViewById(R.id.detailProductName);
+          TextviewProductDescription=findViewById(R.id.detailproductDescription);
+          TextviewPrice=findViewById(R.id.detailPrice);
+          TextviewUploadDate=findViewById(R.id.detailUploadDate);
+          TextviewCategoryName=findViewById(R.id.detailCategoryName);
+          detailImageview=findViewById(R.id.detailImage);
+      }
+}
