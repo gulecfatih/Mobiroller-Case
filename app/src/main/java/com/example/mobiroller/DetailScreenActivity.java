639344abbdac90ff115572a740bdeeb2e694af9c
DESKTOP-POINQD4\gulec <gulecfatih54@gmail.com>
2021-10-16 09:52:11 +0300

    +refactor: contents of the buttons

--- app/src/main/java/com/example/mobiroller/DetailScreenActivity.java
+++ app/src/main/java/com/example/mobiroller/DetailScreenActivity.java
@@ -19,7 +19,7 @@ public class DetailScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_screen);
-
+        
         init();
 
         TextviewProductName.setText(detailProductName);
